ecf8ecc2cf24de2e7486b0a22248bccb68e266cb
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2023-03-27 09:32:03 +0700

    [Add] first commit

---
+++ sensor.install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+agg_conf_name=$1
+
+if [ -z ${agg_conf_name} ];
+then
+    echo "Invalid command, unknow first parameter. Using: ./sensor.install.sh <agg_config_name>"
+else
+    echo "Creating file agg"
+fi
+
+
+cat << EOF > winery-agg.conf
+[agent]
+      hostname = "$agg_conf_name agg"
+  flush_interval = "10s"
+  interval = "10s"
+
+
+# Input Plugins
+[[inputs.cpu]]
+    percpu = true
+    totalcpu = true
+    collect_cpu_time = false
+    report_active = false
+[[inputs.disk]]
+    ignore_fs = ["tmpfs", "devtmpfs", "devfs"]
+[[inputs.io]]
+[[inputs.mem]]
+[[inputs.net]]
+[[inputs.system]]
+[[inputs.swap]]
+[[inputs.netstat]]
+[[inputs.processes]]
+[[inputs.kernel]]
+
+# Output Plugin InfluxDB
+[[outputs.influxdb]]
+  database = "telegraf"
+  urls = [ "http://54.254.47.35:8086" ]
+  username = "telegraf2"
+  password = "5aMWYbKGUv3Q7A4U"
+EOF 
+
+echo "Created file agg.conf !!!"
+
+
+chmod +x telegraf.install.sh
+source telegraf.install.sh
+
+sudo systemctl start telegraf
+sudo systemctl enable telegraf
---
+++ telegraf.install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+agg_conf_place="/etc/telegraf/telegraf.d"
+
+echo "Installing telegraf !"
+wget -q https://repos.influxdata.com/influxdata-archive_compat.key
+echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
+sudo apt-get update && sudo apt-get install telegraf
+echo "Installed telegraf"
+
+echo "Copying file winery-agg.conf to $agg_conf_place"
+cp winery-agg.conf $agg_conf_place
+echo "Copy done !"
